d455b89a5962b6846633c8b110aaca42670f526a
Thomas Birt <69823294+VortexisTV@users.noreply.github.com>
2024-05-29 10:08:12 -0500

    Delete vibeshot directory

--- vibeshot/src/main/java/org/example/vibeshot/Repository/MediaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.vibeshot.Repository;
-
-import org.example.vibeshot.Entity.Media;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MediaRepository extends JpaRepository<Media, Long> {
-}
