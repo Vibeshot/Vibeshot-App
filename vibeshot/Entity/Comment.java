b8442641578e3b57438dc3ef96afd4b32f74309c
shivbrahmbhatt <95452798+shivbrahmbhatt@users.noreply.github.com>
2024-05-29 12:19:23 -0400

    Backend

---
+++ vibeshot/Entity/Comment.java
@@ -0,0 +1,26 @@
+package org.example.vibeshot.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private String text;
+}
